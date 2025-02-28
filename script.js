366bfd6d6bf3a69818852bca5bfecb4567e92e56
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-28 13:29:04 +0400

    ✨ Finish announcements and trending tab

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cardGrid = document.querySelector(".card-grid");
 const projectCard = document.querySelector(".card");
+const trendingCard = document.querySelector("#trending>.side-card")
+const userProfile = document.querySelector(".profile");
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 9; i++) {
   cardGrid.appendChild(projectCard.cloneNode(true))
+
+  i % 3 === 0 ? trendingCard.appendChild(userProfile.cloneNode(true)) : {}
 }
